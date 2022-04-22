2621cf905d9b1a574fc99122f8a67ac6e2f94db8
Cyril-CC <792552360@qq.com>
2022-04-22 12:03:19 +0800

    Feature: add model simplification & compare

---
+++ src/lunarmp/model/ModelCompare.h
@@ -0,0 +1,73 @@
+//
+// Created by Cyril on 2022/4/22.
+//
+
+#ifndef LUNARMP_DEPS_SRC_LUNARMP_MODEL_MODELCOMPARE_H_
+#define LUNARMP_DEPS_SRC_LUNARMP_MODEL_MODELCOMPARE_H_
+
+namespace lunarmp {
+
+class ModelCompare {
+  public:
+    double hausdorff_distance = 0;
+    double hausdorff_bounded_error_distance = 0;
+    double bounded_error_symmetric_Hausdorff_distance = 0;
+    double compare_time = 0;
+
+    /*!
+     * \brief Read file
+     *
+     * \param fileName
+     * \param mesh
+     *
+     */
+    void readFile(std::string fileName, Mesh& mesh);
+
+    /*!
+     * \brief Computes the approximate Hausdorff distance from mesh1 to mesh2
+     *
+     * \param mesh1 The triangle mesh that will be sampled
+     * \param mesh2 The triangle mesh to compute the distance to
+     * \param unit Precision control
+     *
+     */
+    void hausdorffDistance(Mesh& mesh1, Mesh& mesh2, int unit);
+
+    /*!
+     * \brief Computes max approximate Hausdorff distance from mesh1 to mesh2
+     *
+     * \param mesh1 The triangle mesh that will be sampled
+     * \param mesh2 The triangle mesh to compute the distance to
+     * \param error_bound a maximum bound by which the Hausdorff distance estimate is allowed to deviate from the actual Hausdorff distance.
+     *
+     */
+    void boundedErrorSymmetricHausdorffDistance(Mesh& mesh1, Mesh& mesh2, double error_bound);
+
+    /*!
+     * \brief Compare model
+     *
+     * \param baseFile The triangle mesh that will be sampled
+     * \param mesh2 The triangle mesh to compute the distance to
+     * \param error_bound a maximum bound by which the Hausdorff distance estimate is allowed to deviate from the actual Hausdorff distance.
+     *
+     */
+    void modelCompare(std::string baseFile, Mesh& mesh2, double error_bound);
+
+    /*!
+     * \brief Compare model
+     *
+     * \param base The triangle mesh that will be sampled
+     * \param mesh2 The triangle mesh to compute the distance to
+     *
+     */
+    void modelCompare(Mesh& base, Mesh& mesh2, double error_bound);
+
+    /*!
+     * \brief test
+     */
+    void test();
+};
+
+}
+
+#endif  // LUNARMP_DEPS_SRC_LUNARMP_MODEL_MODELCOMPARE_H_
---
+++ src/lunarmp/model/ModelSimplification.h
@@ -0,0 +1,114 @@
+//
+// Created by Cyril on 2022/4/22.
+//
+
+#ifndef LUNARMP_DEPS_SRC_LUNARMP_MODEL_MODELSIMPLIFICATION_H_
+#define LUNARMP_DEPS_SRC_LUNARMP_MODEL_MODELSIMPLIFICATION_H_
+
+#include "ModelBase.h"
+
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Bbox_3.h>
+#include <CGAL/Surface_mesh_simplification/edge_collapse.h>
+
+// edgeCollapseGarlandHeckbert
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_ratio_stop_predicate.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_normal_change_placement.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/GarlandHeckbert_policies.h>
+// edge_collapse_all_short_edges
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_length_cost.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_length_stop_predicate.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Midpoint_placement.h>
+// edge limited
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_stop_predicate.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_cost.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_placement.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_normal_change_filter.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+typedef CGAL::Bbox_3                                               Bbox_3;
+namespace SMS = CGAL::Surface_mesh_simplification;
+
+namespace lunarmp {
+
+class ModelSimplification {
+  public:
+    int remove_edge = 0;              //! The total number of edges removed in the model simplification.
+    double simplification_time = 0.0; //! The time spent simplifying the model.
+
+    /*!
+     * \brief Read file
+     *
+     * \param input_file the name of the output file.
+     * \param mesh A triangular mesh that needs to be simplified.
+     *
+     */
+    bool readFile(std::string input_file, Mesh& mesh);
+
+    /*!
+     * \brief Model simplification based on "Edge_length_stop_predicate" strategy
+     *
+     * \param mesh A triangular mesh that needs to be simplified.
+     * \param count_ratio_stop
+     *
+     */
+    void edgeCollapseGarlandHeckbert(Mesh& mesh, double count_ratio_stop);
+
+    /*!
+     * \brief Model simplification based on "Edge_length_stop_predicate" strategy
+     *
+     * \param mesh A triangular mesh that needs to be simplified.
+     * \param edge_length_limit The limit on the length of the sides of a triangle.
+     *
+     */
+    void edgeCollapseAllShortEdges(Mesh& mesh, double edge_length_limit);
+
+    /*!
+     * \brief Model simplification based on "Edge_length_stop_predicate" strategy
+     *
+     * \param mesh A triangular mesh that needs to be simplified.
+     * \param edge_length_length The limit on the length of the sides of a triangle.
+     *
+     */
+    void edgeCollapseBoundedNormalChange(Mesh& mesh, double edge_length_limit);
+
+    /*!
+     * \brief Calculate scaling factor
+     *
+     * \param mesh A triangular mesh that needs to be simplified.
+     * \param machineBox
+     *
+     */
+    double getDelta(Mesh mesh, double machineBox);
+
+    /*!
+     * \brief Model simplification based on "Edge_length_stop_predicate" strategy
+     *
+     * \param mesh A triangular mesh that needs to be simplified.
+     * \param machine_box Print space range.
+     * \param threshold The minimum side length of a triangle.
+     *
+     */
+    void autoSimplify(Mesh& mesh, double machine_box, double threshold);
+
+    /*!
+     * \brief Simplify model
+     *
+     * \param input_file the name of input file.
+     * \param output_file the name of the output file.
+     * \param type Simplified strategy.
+     * \param machine_box Print space range.
+     * \param threshold The minimum side length of a triangle.
+     *
+     */
+    void modelSimplification(std::string input_file, std::string output_file, int type, double machine_box, double threshold);
+
+};
+
+}
+
+#endif  // LUNARMP_DEPS_SRC_LUNARMP_MODEL_MODELSIMPLIFICATION_H_
