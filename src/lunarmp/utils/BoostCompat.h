f00f60b71ac061a317966e5f122bfcd6850b0dcd
womendoushihaoyin <xyliu_leo@163.com>
2025-04-11 14:37:21 +0800

    push

--- src/lunarmp/utils/BoostCompat.h
+++ src/lunarmp/utils/BoostCompat.h
@@ -3,6 +3,14 @@
 
 #include <iterator>
 
+// 确保引入boost/next_prior.hpp
+#include <boost/next_prior.hpp>
+
+// 如果需要，提供导入的引用
+#ifndef BOOST_ALLOW_DEPRECATED_HEADERS
+#define BOOST_ALLOW_DEPRECATED_HEADERS
+#endif
+
 // 提供兼容较新版本Boost库的函数
 namespace boost {
     template <typename Iterator>
